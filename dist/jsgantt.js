5947b79126ca0d55cb371fd2ee56d328050fb672
AlanNggg <ngpuilam9166@gmail.com>
2021-09-28 21:01:55 +0800

    compile files

--- dist/jsgantt.js
+++ dist/jsgantt.js
@@ -5543,12 +5543,12 @@ exports.computeStartEndDate = function (curStart, curEnd, newStartX, curStartX, 
         newStartDate = date_utils_1.add(curStart, xInUnits, "minute");
         newEndDate = date_utils_1.add(curEnd, x2InUnits, "minute");
     }
-    newStartDate = new Date(newStartDate.getTime() - newStartDate.getTimezoneOffset() * 60000)
+    newStartDate = new Date(new Date(newStartDate.getTime() - newStartDate.getTimezoneOffset() * 60000)
         .toISOString()
-        .split("T")[0];
-    newEndDate = new Date(newEndDate.getTime() - newEndDate.getTimezoneOffset() * 60000)
+        .split("T")[0]);
+    newEndDate = new Date(new Date(newEndDate.getTime() - newEndDate.getTimezoneOffset() * 60000)
         .toISOString()
-        .split("T")[0];
+        .split("T")[0]);
     return { newStartDate: newStartDate, newEndDate: newEndDate };
 };
 exports.getOffset = function (pStartDate, pEndDate, pColWidth, pFormat, pShowWeekends) {
